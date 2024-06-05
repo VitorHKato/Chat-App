c1bc1ce36a8d50b23917a61072fe4a3a37618b6a
Vitor Kato <vitor48kato@gmail.com>
2024-06-04 23:59:56 -0300

    Add decryption and sign

--- src/Group.js
+++ src/Group.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import Input from "./components/Input";
+import {JSEncrypt} from "jsencrypt";
+import { AES } from 'crypto-js';
+import CryptoJS from 'crypto-js';
 
 export default function Group({ data }) {
     const [messages, setMessages] = useState([]);
@@ -8,11 +11,54 @@ export default function Group({ data }) {
     const [newMessage, setNewMessage] = useState('');
     const groupName = data.group;
     const username = data.username;
-    const baseUrl = 'http://140.238.182.69:55667/'
-    //const baseUrl = 'http://localhost:55667/'
+    //const baseUrl = 'http://140.238.182.69:55667/'
+    const baseUrl = 'http://localhost:55667/'
+
+    const [privateKey, setPrivateKey] = useState('');
+    const [serverPublicKey, setServerPublicKey] = useState('');
+
+    const generateKeys = () => {
+        const jsEncrypt = new JSEncrypt({default_key_size: 2048})
+        setPrivateKey(jsEncrypt.getPrivateKey());
+    }
+
+    const decryption = (body) => {
+        // var privateKey = '-----BEGIN RSA PUBLIC KEY-----\n' +
+        //     'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoPd+Df9O7J0gXF5aWDxg\n' +
+        //     'Hvv9baSg7wivjbzstyrjDgYUgvjq8FkZv9NYBVKE7ysYTFvuj6/zY7JlgbJawqsF\n' +
+        //     'Q8yoArubvVswFzkjVe/QAUh/dthw5VWOqLuBX8DevE0izylIBsgDaLm9HvLcNjyE\n' +
+        //     'WqT9qGQfdLDxye2kKDd/x65BwybrCIo7AqW09UAWsrS6B3cjdW17GGUhkLKWfAWV\n' +
+        //     'i5WeSi5nX3dJ1UA/jgYCfymABeMF3wSCWl3JLeDRIoMm6XlUpKQgcT7HPD4BG8Qx\n' +
+        //     'YW8uZc/HXTE0Yc41VUrjhB6UYy9EJeUgCEB8LgW0sxbup0gvycciXO4JKXvqBeBi\n' +
+        //     'QwIDAQAB\n' +
+        //     '-----END RSA PUBLIC KEY-----';
+        const bytes = AES.decrypt(body, serverPublicKey);
+        return bytes.toString();
+    }
+
+    const signBody = (body) => {
+        const encrypt = new JSEncrypt();
+        encrypt.setPrivateKey(privateKey);
+        encrypt.sign(body, CryptoJS['sha256'], (signature) => {
+            // Handle the signature here
+            console.log("Signature:", signature);
+        });
+    }
+
+    const fetchServerPublicKey = async () => {
+        try {
+            const res = await fetch(`${baseUrl}key/`);
+            const serverKey = await res.text();
+            setServerPublicKey(serverKey);
+        } catch (error) {
+            console.error('Error fetching server public key: ', error);
+        }
+    };
 
     useEffect(() => {
-        fetchMessages();
+        generateKeys();
+        fetchServerPublicKey();
+        setInterval(fetchMessages, 5000);
     }, [groupName]);
 
     const isUserMessage = (id) => {
@@ -23,8 +69,13 @@ export default function Group({ data }) {
         try {
             const res = await fetch(`${baseUrl}messages/${groupName}`);
             const response = await res.json();
+
+            response.map(msg => decryption(msg.Content))
+            response.map(msg => signBody(msg.Content))
+
             setMessages(response);
             setUserMessages(response.filter(msg => msg.SenderId === username));
+
         } catch (error) {
             console.error('Error fetching messages: ', error);
         }
